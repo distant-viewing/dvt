dea8bbff2f798e1a480123a28016cb4ac8dce062
Taylor Arnold <taylor@dvlab.io>
2023-02-11 21:32:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ required = [
 
 setup(
     name="dvt",
-    version="1.0.1",
+    version="1.0.2",
     description="Computational Analysis of Visual Culture",
     long_description=long_description,
     author="Taylor Anold, Lauren Tilton",
